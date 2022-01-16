85135899502c18c5f357e6a3e659c8e30bb6f93f
jeongbug <wndud0249@naver.com>
2022-01-16 21:12:16 +0900

    initial commit -- nextjs, typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    REACT_APP_ENCRYPT_KEY: 'NIMDA_IRACOP_DBDMSWJD',
+    REACT_APP_DEVELOPMENT_API: 'https://cil-client-api.dramatic.one/',
+    REACT_APP_PRODUCTION_API: 'https://cil-client-api.dramatic.one/',
+  },
+};
