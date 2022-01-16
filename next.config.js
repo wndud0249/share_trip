ef775bb40d246d9138b3f57c9dbbafaa2ae13ce6
jeongbug <wndud0249@naver.com>
2022-01-16 21:15:43 +0900

    git ignore 다시 적용

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    REACT_APP_ENCRYPT_KEY: 'NIMDA_IRACOP_DBDMSWJD',
-    REACT_APP_DEVELOPMENT_API: 'https://cil-client-api.dramatic.one/',
-    REACT_APP_PRODUCTION_API: 'https://cil-client-api.dramatic.one/',
-  },
-};
